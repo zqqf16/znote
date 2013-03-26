2626c366033258e4e3c4596110460c6d544f9a86
zorro <zqqf16@gmail.com>
2013-03-26 19:21:33 +0800

    Add interface for get data

---
+++ api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from module import * 
+
+class Api():
+    def __init__(self, db):
+        self.db = db
+
+    def get_articles(self, type='publish'):
+        return self.db.query(Article).filter(Article.status==type).all()
+
+    def get_article_by_id(self, article_id):
+        return self.db.query(Article).get(article_id)
+
+    def get_article_by_slug(self, article_slug):
+        return self.db.query(Article).filter(Article.slug==article_slug).first()
+
+    def get_categories(self):
+        return self.db.query(Category).all()
+
+    def get_category_by_id(self, category_id):
+        return self.db.query(Category).get(category_id)
+
+    def get_category_by_name(self, category_name):
+        return self.db.query(Category).filter(Category.name==category_name).first()
